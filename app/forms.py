11c83bc838d82e5fe022c05f0aadf7964c472bcd
alevis <lagaba@umass.edu>
2017-01-15 23:55:46 -0500

    5 monites

--- app/forms.py
+++ app/forms.py
@@ -5,3 +5,24 @@ from wtforms.validators import DataRequired
 class LoginForm(FlaskForm):
 	openid = StringField('openid',validators=[DataRequired()])
 	remember_me = BooleanField('remember_me',default=False)
+
+class EditForm(form):
+    nickname = StringField('nickname',validators=[DataRequired()])
+    about_me = TextAreaField('about_me',validators=[Length(min=0,max=140)])
+
+    def __init__(self,original_nickname,*args, **kwargs)
+        Form.__init__(self,*args,**kwargs)
+        self.original_nickname = original_nickname
+
+    def validate(self):
+        if not Form.validate(self):
+            return False
+        if self.nickname.data == self.original_nickname:
+            return True
+        user = User.query.filter_by(nickname=self.nickname.data).first()
+        if user != None:
+            self.nickname.errors.append('This nickname is already in use. Please chose another one.')
+            return False
+        return True
+
+
