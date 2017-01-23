ecf10f413b60c84c19add93c676066299c60375b
alevis <lagaba@umass.edu>
2017-01-22 23:48:47 -0500

    listening to tamia

--- app/forms.py
+++ app/forms.py
@@ -1,16 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms import StringField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
 class LoginForm(FlaskForm):
 	openid = StringField('openid',validators=[DataRequired()])
 	remember_me = BooleanField('remember_me',default=False)
 
-class EditForm(form):
+class EditForm(FlaskForm):
     nickname = StringField('nickname',validators=[DataRequired()])
     about_me = TextAreaField('about_me',validators=[Length(min=0,max=140)])
 
-    def __init__(self,original_nickname,*args, **kwargs)
+    def __init__(self,original_nickname,*args, **kwargs):
         Form.__init__(self,*args,**kwargs)
         self.original_nickname = original_nickname
 
