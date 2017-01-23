ca89115342530a2462a714c2e29b3e66c0b371b4
alevis <lagaba@umass.edu>
2017-01-23 04:16:57 -0500

    avatars

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
-from .forms import LoginForm
+from .forms import LoginForm, EditForm
 from .models import User
 from app import app
 
@@ -15,7 +15,7 @@ def before_request():
             g.user.last_seen = datetimfe.utcnow()
             db.session.add(g.user)
             db.session.commit()
-		
+
 @app.route('/')
 @app.route('/index')
 @login_required
@@ -88,6 +88,22 @@ def logout():
 	logout_user()
 	return redirect(url_for('index'))
 
+@app.route('/edit',methods=['GET','POST'])
+@login_required
+def edit():
+    form = EditForm()
+    if form.validate_on_submit():
+        g.user.nickname = form.nickname.data
+        g.user.about_me = form.about_me.data
+        db.session.add(g.user)
+        db.session.commit()
+        flash('Your change have been saved.')
+        return redirect(url_for('eidt'))
+    else:
+        form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+    return render_template('edit.html',form=form)
+
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'),404
@@ -99,4 +115,4 @@ def internal_error(error):
 
 @lm.user_loader
 def load_user(id):
-	return User.query.get(int(id))	
+	return User.query.get(int(id))
