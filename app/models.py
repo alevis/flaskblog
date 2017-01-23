8d8e9ee5e60ea73bfbd3c9d429250c2d527defd1
alevis <lagaba@umass.edu>
2017-01-22 23:21:03 -0500

    avatars

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
+import hashlib
 from app import db
+from hashlib import md5
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -23,7 +25,9 @@ class User(db.Model):
 			return unicode(self.id) # python 2
 		except NameError:
 			return str(self.id)	# python 3
-    
+    def avatar(self, size):
+        return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % (md5(self.email.encode('utf-8')).hexdigest(),size)
+
     @staticmethod
     def make_unique_nickname(nickname):
         if User.query.filter_by(nickname=nickname).first() is None:
