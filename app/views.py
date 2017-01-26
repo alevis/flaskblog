a1edfe3185e52ddf3c7077792ecbc134150315e5
alevis <lagaba@umass.edu>
2017-01-25 22:07:53 -0500

    added posform, need to review somethings

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
-from .forms import LoginForm, EditForm
-from .models import User
+from .forms import LoginForm, EditForm, PostForm
+from .models import User, Post
 from app import app
 
 
